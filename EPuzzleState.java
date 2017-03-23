77d744c40b8c379bdffbcc9cb978f4a7f3808be0
Abaii <jabedmund1@sheffield.ac.uk>
2017-03-23 14:37:03 +0000

    Add files via upload

--- EPuzzleState.java
+++ EPuzzleState.java
@@ -1,3 +1,4 @@
+import java.util.*;
 
 public class EPuzzleState extends SearchState {
 	// display the board as an array
@@ -6,7 +7,7 @@ public class EPuzzleState extends SearchState {
 	// constructor
 	public EPuzzleState(int[][] b) {
 		this.board = b;
-		
+
 	}
 
 	// accessor for puzzle state
@@ -15,7 +16,7 @@ public class EPuzzleState extends SearchState {
 	}
 
 	// goal method
-	public boolean goalP(Searcher searcher) {
+	public boolean goalP(Search searcher) {
 		PuzzleSearch pS = (PuzzleSearch) searcher;
 		int[][] target = pS.getTarget(); // get target amount
 		return (target == board); // return if, may have to look up the syntax
@@ -23,69 +24,92 @@ public class EPuzzleState extends SearchState {
 	}
 
 	// same state
-	public int[] SameState(SearchState s) {
-		PuzzleState s1 = (PuzzleState) s;
-		return (board = s1.getBoard());
+	public boolean sameState(SearchState s) {
+		EPuzzleState s1 = (EPuzzleState) s;
+		return (board == s1.getBoard());
 	}
 
-	public <ArrayList> getSuccessors(Searcher searcher){
-		PuzzleSearch pS = (PuzzleSearch) searcher; 
-		int tS = pS.getTarget(); //get the target
-		int[][] copyBoard = EPuzzleState.getBoard(); //copy of the board 
-		int[][] tempBoard = EPuzzleState.getBoard(); //used to swap two board values
-		int xGap; //x co-ord of the gap
-		int yGap; //y co-ord of the gap
+	public ArrayList<SearchState> getSuccessors(Search searcher){
+		PuzzleSearch pS = (PuzzleSearch) searcher;
+		//int tS = pS.getTarget(); //get the target
+		EPuzzleState cB = new EPuzzleState(board);
+		int[][] copyBoard = cB.getBoard();
+		int[][] p1 = copyBoard;
+		int[][] p2 = copyBoard;
+		int[][] p3 = copyBoard;
+		int[][] p4 = copyBoard;
+		//print the board -- TEST
+		for(int i = 0; i < copyBoard.length ; i++){
+			for (int j = 0; j < copyBoard.length; j++){
+				System.out.println(board[i][j]);
+			}
+		}
+
+
+
+		int xGap=0; //x co-ord of the gap
+		int yGap=0; //y co-ord of the gap
 		ArrayList<EPuzzleState> pslis=new ArrayList<EPuzzleState>(); // the list of puzzle states
 		ArrayList<SearchState> slis=new ArrayList<SearchState>();
 		//find the empty space, gap, in the board
 		for (int i = 0; i < copyBoard.length; i++){
 			for (int j = 0; j < copyBoard.length; j++){
-				if (copyBoard[i][j]) == 0){
-					xGap = i; 
+				if (copyBoard[i][j] == 0){
+					xGap = i;
 					yGap = j;
+					System.out.println("xGap = " + xGap);
+					System.out.println("yGap = "+yGap);
+
 				}
 			}
 		}
-		//POSSIBLE WAY OF SOlVING 
+		//POSSIBLE WAY OF SOlVING
 		//create 4 additional boards, P1, P2, P3, P4
-		//at each of the boundary cases, edit one board per boundary case 
-		//pass the array as a parameter in EPuzzleState 
+		//at each of the boundary cases, edit one board per boundary case
+		//pass the array as a parameter in EPuzzleState
 		//eg pslis.add(newEPuzzleState[P1]);
 		//boundary cases, when y != 2, x !=2, y !=0, x!= 0
-		
-		//possible moves at any given state 
+
+		//possible moves at any given state
 			if (xGap != 0 ){
-				copyBoard[xGap][yGap] = copyBoard[xGap + 1][yGap];
-				copyBoard[xGap  +1][yGap] = 0;
-				pslis.add(new EPuzzleState(copyBoard));
+				p1[xGap][yGap] = p1[xGap - 1][yGap];
+				p1[xGap  -1][yGap] = 0;
+				pslis.add(new EPuzzleState(p1));
+				System.out.println("x !=0 reached");
 			}
 			if (xGap != 2){
-				copyBoard[xGap][yGap] = copyBoard[xGap -1][yGap];
-				copyBoard[xGap -1][yGap] = 0;
-				pslis.add(new EPuzzleState(copyBoard));
+				p2[xGap][yGap] = p2[xGap +1][yGap];
+				p2[xGap +1][yGap] = 0;
+				pslis.add(new EPuzzleState(p2));
+
+				System.out.println("x !=2 reached");
 			}
 			if (yGap != 0){
-				copyBoard[xGap][yGap] = copyBoard[xGap][yGap +1];
-				copyBoard[xGap][yGap+1] =0;
-				pslis.add(new EPuzzleState(copyBoard));
+				p3[xGap][yGap] = p3[xGap][yGap -1];
+				p3[xGap][yGap-1] =0;
+				pslis.add(new EPuzzleState(p3));
+
+				System.out.println("y !=0 reached");
 			}
 			if(yGap != 2){
-				copyBoard[xGap][yGap] = copyBoard[xGap][yGap -1];
-				copyBoard[xGap][yGap-1]=0;
-				pslis.add(new EPuzzleState(copyBoard));
+
+				p4[xGap][yGap] = p4[xGap][yGap+1];
+				p4[xGap][yGap+1]=0;
+				pslis.add(new EPuzzleState(p4));
+
+				System.out.println("y !=2 reached");
 			}
-			
+
 			//cast puzzle states to search states for slis
 			for (EPuzzleState ps:pslis)slis.add((SearchState)ps);
 			return slis;
 		}
 	public String toString(){
-		for(int i = 0; i <board.length-1;i++){
-			for(int j = 0; j < board.length -1; j++){
-				return "Puzzle State: " + board[i][j];
+		for(int i = 0; i <board.length;i++){
+			for(int j = 0; j < board.length ; j++){
+				return "board" + board[i][j];
 			}
 		}
+		return "No Puzzle state";
 	}
-}
-
 }
--- PuzzleSearch.java
+++ PuzzleSearch.java
@@ -1,28 +1,21 @@
 
 public class PuzzleSearch extends Search {
 	// instance variables
-	//private int[] board = new int[];
+
 	private int[][] target;
-	//private int gap;
+
 
 	// constructor
-	public PuzzleSearch(int t //int g) {
-		//board = b (may need to add this back in, I'm not sure if the board state needs to be here
-		target = t;
-		//gap = g;
+	public PuzzleSearch(int[][] t) {
+		this.target = t;
+
 	}
 
-	// accessors
-	public int[] getBoard() {
-		return board;
-	}
+	// accessor
 
-	public int getTarget() {
+	public int[][] getTarget() {
 		return target;
 	}
 
-	public int getGap() {
-		return gap;
-	}
 
 }
